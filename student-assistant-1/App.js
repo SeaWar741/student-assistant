2affb1c4c600119a33a33f78618a49a9c55d5498
SeaWar741 <juanca741@gmail.com>
2018-05-13 04:14:17 -0500

    iniciando proyecto, react ya funciona

---
+++ student-assistant-1/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text} from 'react-native';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon} from 'native-base';
+import { Font, AppLoading } from "expo";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Text>Ya funciona REACT!!!!</Text>
+      </Container>
+    );
+  }
+}
