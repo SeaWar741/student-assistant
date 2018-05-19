2b400a63cdfc2f2921ea2e4f238f2e8a0d408181
SeaWar741 <juanca741@gmail.com>
2018-05-18 21:00:37 -0500

    iniciando homescreen

--- student-assistant-1/App.js
+++ student-assistant-1/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text} from 'react-native';
+import React, { Component } from 'react';
+import { Text} from 'react-native';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon} from 'native-base';
 import { Font, AppLoading } from "expo";
 
 export default class App extends React.Component {
   render() {
-    return (
-      <Container>
-        <Text>Ya funciona REACT!!!!</Text>
-      </Container>
-    );
+      return (
+        <Container>
+          <Header>
+            <Left>
+              <Button transparent>
+                <Icon name='menu' />
+              </Button>
+            </Left>
+            <Body>
+              <Title>Student Assistant</Title>
+            </Body>
+            <Right />
+          </Header>
+          <Content>
+            <Text>
+              Aqui se insertará el menú
+            </Text>
+          </Content>
+          <Footer>
+            <FooterTab>
+              <Button iconLeft>
+                <Icon name='home' />
+              </Button>
+            </FooterTab>
+          </Footer>
+        </Container>
+      );
   }
 }
