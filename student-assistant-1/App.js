9adc0699fc925a931b18e081e778a8749528e9a8
SeaWar741 <juanca741@gmail.com>
2018-05-24 14:08:52 -0500

    Bottom menu done, fix botton position and fix topmargin(possible safeview area)

--- student-assistant-1/App.js
+++ student-assistant-1/App.js
@@ -1,36 +1,55 @@
 import React, { Component } from 'react';
-import { Text} from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon} from 'native-base';
 import { Font, AppLoading } from "expo";
 
 export default class App extends React.Component {
   render() {
       return (
-        <Container>
-          <Header>
-            <Left>
-              <Button transparent>
-                <Icon name='menu' />
-              </Button>
-            </Left>
-            <Body>
-              <Title>Student Assistant</Title>
-            </Body>
-            <Right />
-          </Header>
-          <Content>
-            <Text>
-              Aqui se insertará el menú
-            </Text>
-          </Content>
-          <Footer>
-            <FooterTab>
-              <Button iconLeft>
-                <Icon name='home' />
-              </Button>
-            </FooterTab>
-          </Footer>
-        </Container>
+          <Container style={styles.safeArea}>
+            <Header>
+              <Left>
+                <Button transparent>
+                  <Icon name='menu' />
+                </Button>
+              </Left>
+              <Body>
+                <Title>Student Assistant</Title>
+              </Body>
+              <Right />
+            </Header>
+            <Content>
+              <Text>
+                Aqui se insertará el menú
+              </Text>
+            </Content>
+            <Footer>
+              <FooterTab>
+                <Button iconLeft>
+                  <Icon name='home' />
+                </Button>
+                <Button iconLeft>
+                  <Icon name='clock' />
+                </Button>
+                <Button iconLeft>
+                  <Icon name='list-box' />
+                </Button>
+                <Button iconLeft>
+                  <Icon name='calendar' />
+                </Button>
+                <Button iconLeft>
+                  <Icon name='more' />
+                </Button>
+              </FooterTab>
+            </Footer>
+          </Container>
       );
   }
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+      flex: 1,
+      backgroundColor: '#ddd'
+  }
+});
